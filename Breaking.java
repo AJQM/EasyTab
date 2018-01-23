9e893b49bb77bce933dda3c1985729e8b62670d7
AJQM <33493537+AJQM@users.noreply.github.com>
2018-01-22 21:51:59 -0500

    Add files via upload

--- Breaking.java
+++ Breaking.java
@@ -1,34 +1,15 @@
 import java.util.*;
 
 public interface Breaking {
-	/**
-	 * keeps track of the teams still in the tournament
-	 * 
-	 * 
-	 */
-	/**
-	 * The original teams in the
-	 * 
-	 * @return
-	 */
-
-	/**
-	 * 
-	 * 
-	 * @param teamsList
-	 *            a sorted list of Team objects
-	 * @return
-	 */
-	;
 
 	/**
 	 * precondition: teamsList is sorted in order of rank, with TeamsList.get(0)
 	 * being last place, etc. postCondition:given the total sorted list of teams
-	 * (teamsList): asks the user how many teams are breaking, makes a new
+	 * (teamsList): asks the user how many <code> Team</code>s are breaking, makes a new
 	 * ArrayList<Team> with only teams breaking, puts that in breakingTeams AND
 	 * originalBreak (originalBreak will never be changed), and puts the number of
 	 * breakrounds into breakRounds, breakrounds = (int) (1.4427 * Math.log(amount
-	 * teams Breaking); initializes int breakRounds public final ArrayList<Team>
+	 * teams Breaking); initializes int <b>breakRounds</b> public final ArrayList<Team>
 	 * originalBreak; public ArrayList<Team> breakingTeams;
 	 * 
 	 * @param teamsList
@@ -56,15 +37,14 @@ public interface Breaking {
 	 * breakingTeams so that it is in the order of pairings: e.g.pairings[0][0] is
 	 * element 1,pairings[0][1] is element 2,pairings[1][0] is element 3 etc.
 	 * 
-	 * @param pairings:
+	 * Asks the user for each pairing (each pairing is a row), which team won
+	 * postCondition: breakingTeams has all the losing teams deleted
+	 * 
+	 * @param pairings
 	 *            the 2D array computed in breakRound, where Each row has one team
 	 *            in col 0 and the other team in col 1
-	 */
-	public void sortBreak(Team[][] pairings);
-
-	/**
-	 * // Asks the user for each pairing (each pairing is a row), which team won //
-	 * postCondition: breakingTeams has all the losing teams deleted /**
+	 * 
+	 * 
 	 */
 	public void breakResults(Team[][] pairings);
 
@@ -72,7 +52,6 @@ public interface Breaking {
 	 *
 	 * @return originalBreak
 	 */
-
 	public ArrayList<Team> getOriginalBreak(); // returns originalBreak
 
 	/**
